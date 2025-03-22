2873a937b66a56e63fcb0f9083f81950e1bc4489
Ash <anthony.russo@slu.edu>
2025-03-22 10:31:12 -0500

    added more test messages

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 function love.load()
     font = love.graphics.newFont(24)
+    print("test") -- console printing test
 end
 
 
@@ -10,6 +11,6 @@ end
 function love.draw()
     love.graphics.setColor(1,1,1,1) -- DONT DELETE, COULD PREENT ANYTHING FROM BEING DRAWN
     love.graphics.print("test", font, 150, 150)
-    print("test") -- console printing test
+    
 
 end
