84a10b837bb80d8dda052c07052b61943dfc27bc
Rojoloco04 <parrackjack@gmail.com>
2025-03-22 11:00:59 -0500

    add basic task functionality

---
+++ task.lua
@@ -0,0 +1,29 @@
+Task = {}
+Task.__index = Tasks
+
+function Task:add(name, dueDate, description)
+    local instance = {}
+    setmetatable(instance,Task)
+
+    name = name or "Unnamed task"
+    dueDate = dueDate or "No due date"
+    description = description or "No description"
+    instance.completed = false
+
+    return instance
+end
+
+
+function Task:complete()
+    self.completed = true
+    print("Task " .. task.name .. " marked as completed.")
+end
+
+function Task:display()
+    print("Task: " .. task.name)
+    print("Due date: " .. task.date)
+    print("Description: " .. task.description)
+    print("Status: " .. task.completed)
+end 
+
+local task1 = Task:new("Finish project", "12/12/12", "HackSLU")
