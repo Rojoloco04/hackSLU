b3f8a997a964756de7e52cbbaf61edd3d83a7017
Phlabry <skhvadagadze@gmail.com>
2025-03-22 11:58:14 -0500

    xp gain

--- Source/task.lua
+++ Source/task.lua
@@ -1,32 +1,25 @@
 Task = {}
-Task.__index = Tasks
+Task.__index = Task
 
-function Task:add(name, dueDate, description)
+function Task.new(name)
     local instance = {}
     setmetatable(instance,Task)
 
-    name = name or "Unnamed task"
-    dueDate = dueDate or "No due date"
-    description = description or "No description"
+    instance.name = name or "Unnamed task"
     instance.completed = false
 
     return instance
 end
 
-
 function Task:complete()
     local utils = require("level")
     self.completed = true
-    print("Task " .. task.name .. " marked as completed.")
+    print("Task " .. self.name .. " marked as completed.")
 
     utils.xpGain(25)
 end
 
 function Task:display()
-    print("Task: " .. task.name)
-    print("Due date: " .. task.date)
-    print("Description: " .. task.description)
-    print("Status: " .. task.completed)
-end 
-
-local task1 = Task:new("Finish project", "12/12/12", "HackSLU")
+    print("Task: " .. self.name)
+    print("Status: " .. tostring(self.completed))
+end
