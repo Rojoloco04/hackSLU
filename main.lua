14302ad106c853b01c2793ad84d1fcf0d0c38e9e
Rojoloco04 <parrackjack@gmail.com>
2025-03-22 12:38:27 -0500

    delete taskmanager, fix main

--- main.lua
+++ main.lua
@@ -1,14 +1,29 @@
+require("textbox")
+require("task")
+taskList = {}
+
 function love.load()
     font = love.graphics.newFont(24)
     print("test") -- console printing test
+    testTextbox = textbox.create(150,150,1000, 16)
 end
 
 
 function love.update(dt)
-
+    testTextbox:update()
 end
 
 function love.draw()
     love.graphics.setColor(1,1,1,1) -- DONT DELETE, COULD PREENT ANYTHING FROM BEING DRAWN
-    love.graphics.print("test", font, 150, 150)
+    --love.graphics.print("test", font, 150, 150)
+    testTextbox:draw()
+
+end
+
+
+function love.mousepressed(x,y,button)
+    testTextbox:mousepressed(x,y,button)
+end
+function love.keypressed(key)
+    testTextbox:keypressed(key)
 end
