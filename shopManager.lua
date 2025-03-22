019ce389d721d4b21ff34228f3f21f88033dca44
Rojoloco04 <parrackjack@gmail.com>
2025-03-22 18:07:02 -0500

    update shopmanager

--- shopManager.lua
+++ shopManager.lua
@@ -4,8 +4,11 @@ require("data")
 
 function purchase(item)
     local money = getMoney()
-    if money >= item.price then
+    if item.bought == true then
+        print("Item has already been purchased")
+    elseif money >= item.price then
         updateMoney("sub", item.price)
+        item.bought = true
         print("Item " .. item.name .. " purchased for " .. item.price .. " Flex.")
     else
         print("Insufficient funds.")
