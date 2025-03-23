3639c03d09ed7f2af68bd3bb5bca3829a145ce26
Rojoloco04 <parrackjack@gmail.com>
2025-03-23 03:04:38 -0500

    oops

--- DisplayData/displayDataMain.lua
+++ DisplayData/displayDataMain.lua
@@ -5,7 +5,6 @@ function displayDataMain()
     love.graphics.setColor(0,0,0)
     local basey = 331
     local gap = 6
-    print("Current taskList:")
     for i, task in ipairs(taskList) do
         love.graphics.print(task.name, font, 40, basey + (i - 1) * (78 + gap))
     end
